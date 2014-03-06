91e248c78c0ccddfdf7b3b27606e97230d8bd2aa
Qambar <qambar.raza@hotmail.com>
2014-03-06 14:04:33 +0000

    flower are buttons now

--- js/script.js
+++ js/script.js
@@ -3,29 +3,38 @@
  */
 
 function yellowFlower() {
-	flyOnFlower("200px", "632px");
+    flyOnFlower("200px", "632px");
 }
 
 function whiteFlower() {
-	flyOnFlower("350px", "132px");
+    flyOnFlower("350px", "132px");
 }
 
 function pinkFlower() {
-	flyOnFlower("600px", "732px");
+    flyOnFlower("600px", "732px");
 }
 
-function flyOnFlower(x,y) {
-	insectSound.currentTime = 0;
-	insectSound.play();
-	TweenLite.to(insect, 2, {top: x, left: y, delay:1, scale:2});
+function flyOnFlower(x, y) {
+    try {
+        insectSound.currentTime = 0;
+        insectSound.play();
+    } catch (e) {
 
+    }
+    TweenLite.to(insect, 2, {
+        top: x,
+        left: y,
+        delay: 1,
+        scale: 2
+    });
+
+
+    // setTimeout(function() {
+    // 	TweenLite.to(insect, 2, {top: "-830px", delay:1, scale:0.2});
+    // }, 3000);
 
     setTimeout(function() {
-    	TweenLite.to(insect, 2, {top: "-830px", delay:1, scale:0.2});
-    }, 3000);
-
-    setTimeout(function() {
-    	insectSound.pause();
+        insectSound.pause();
 
     }, 2000);
 }
